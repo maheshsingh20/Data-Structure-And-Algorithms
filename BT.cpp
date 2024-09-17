b9c176aedcc8c5c09314e5eb7437a70a12a3a4dc
maheshsingh20 <singhmahesh2924@gmail.com>
2024-09-17 21:36:30 +0530

    Updated bt.cpp with recent changes

--- BT.cpp
+++ BT.cpp
@@ -131,16 +131,123 @@
 // }
 
 
-
-
-
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <queue>
 using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node* btrec(){
+    int data;
+    cout << "Enter data (-1 for no node): ";
+    cin >> data;
+    
+    if(data == -1){
+        return NULL;
+    }
+    
+    node* root = new node(data);
+    
+    cout << "Enter data for left of " << data << ": ";
+    root->left = btrec();
+    
+    cout << "Enter data for right of " << data << ": ";
+    root->right = btrec();
+    
+    return root;
+}
+
+void levelordertraversal(node* root){
+    if(root == NULL) return;
+
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL); // Marker for new level
+    
+    while (!q.empty()){
+        node* temp = q.front();
+        q.pop();
+        
+        if(temp == NULL){
+            cout << endl;
+            if(!q.empty()){
+                q.push(NULL); // Add marker for next level
+            }
+        }else{
+            cout << temp->data << " ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+bool solveforLCA(node* root, int n, vector<int> &path){
+    if(root==NULL){
+        return false;
+    }
+    path.push_back(root->data);
+    if(root->data==n) return true;
+    int leftt=solveforLCA(root->left,n,path);
+    int rightt=solveforLCA(root->right,n,path);
+    if(leftt||rightt){
+        return true;
+    }
+    path.pop_back();
+    return false;
+}
+
+vector<int> pathofnode(node* root, int k){
+    vector<int> path;
+    solveforLCA(root, k,path);
+    return path;
+}
+
+int lowestCommonAncestor(node* root, int k ,int j){
+    vector<int> path1;
+    vector<int> path2;
+
+    solveforLCA(root ,k,path1);
+    solveforLCA(root ,j,path2);
+    int LCA=0;
+    for(int i=0,j=0;i<path1.size() && j<path2.size();i++,j++){
+        if(path1[i]!=path2[j]){
+            break;
+        }else{
+            LCA=path1[i];
+        }
+    }
+    return LCA;
+}
 
 
 
 int main(){
-vector <int> nodes={1,2,3,4,5,6,7};
-node* root=buildtree(nodes);
+    node* root = btrec();
+    cout << "Level Order Traversal:" << endl;
+    levelordertraversal(root);
+    int res=lowestCommonAncestor(root, 8, 7);
+    cout << "Lowest Common Ancestor: " << res << endl;
+    cout<<"Path of node is: ";
+    vector<int> path = pathofnode(root, 6);
+    for (int i = 0; i < path.size(); i++) {
+        cout << path[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
