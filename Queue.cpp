e395900f7b2f9b227876e3631681561833c87bae
maheshsingh20 <singhmahesh2924@gmail.com>
2024-09-19 12:39:29 +0530

    Updated bt.cpp with recent changes

--- Queue.cpp
+++ Queue.cpp
@@ -1,1 +1,43 @@
-#in
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    // Input operator from user
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    // Input two numbers from user
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    // Perform the operation based on the operator
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                result = num1 / num2;
+            else {
+                printf("Error! Division by zero.\n");
+                return -1;
+            }
+            break;
+        default:
+            printf("Error! Invalid operator.\n");
+            return -1;
+    }
+
+    // Output the result
+    printf("Result: %.2lf\n", result);
+
+    return 0;
+}
